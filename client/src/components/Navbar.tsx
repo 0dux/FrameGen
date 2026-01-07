395129e0124b8a23245745df6d1eba242659168a
Daksh Yadav <dakshdav@gmail.com>
2026-01-07 23:02:49 +0530

    Navbar complete

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,14 +1,12 @@
 import { MenuIcon, XIcon } from "lucide-react";
 import { motion } from "motion/react";
 import { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { navlinks } from "../data/navlinks";
-import type { INavLink } from "../types";
+import { Link, useNavigate } from "react-router-dom";
 import SvgLogo from "./SvgLogo";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-
+  const navigate = useNavigate();
   return (
     <>
       <motion.nav
@@ -21,25 +19,43 @@ export default function Navbar() {
         <Link to={"/"}>
           <SvgLogo
             className={
-              "h-8 w-fit hover:scale-105 transition duration-500 overflow-hidden"
+              "h-12 w-auto active:scale-95 transition-all duration-200"
             }
           />
         </Link>
 
         <div className="hidden md:flex items-center gap-8 transition duration-500">
-          {navlinks.map((link: INavLink) => (
-            <NavLink
-              key={link.name}
-              to={link.href}
-              className="hover:text-pink-500 transition"
-            >
-              {link.name}
-            </NavLink>
-          ))}
+          <Link
+            to={"/"}
+            className="hover:text-pink-500 hover:font-medium active:scale-95 transition-all duration-200 overflow-hidden"
+          >
+            Home
+          </Link>
+          <Link
+            to={"/generate"}
+            className="hover:text-pink-500 hover:font-medium active:scale-95 transition-all duration-200 overflow-hidden"
+          >
+            Generate
+          </Link>
+          <Link
+            to={"/my-generation"}
+            className="hover:text-pink-500 hover:font-medium active:scale-95 transition-all duration-200 overflow-hidden"
+          >
+            My Generations
+          </Link>
+          <Link
+            to={"#"}
+            className="hover:text-pink-500 hover:font-medium active:scale-95 transition-all duration-200 overflow-hidden"
+          >
+            My Contacts
+          </Link>
         </div>
 
-        <button className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
-          Start free trial
+        <button
+          onClick={() => navigate("/login")}
+          className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full"
+        >
+          Get Started!
         </button>
         <button onClick={() => setIsOpen(true)} className="md:hidden">
           <MenuIcon size={26} className="active:scale-90 transition" />
@@ -51,15 +67,41 @@ export default function Navbar() {
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        {navlinks.map((link: INavLink) => (
-          <NavLink
-            key={link.name}
-            to={link.href}
-            onClick={() => setIsOpen(false)}
-          >
-            {link.name}
-          </NavLink>
-        ))}
+        <Link
+          onClick={() => setIsOpen(false)}
+          to={"/"}
+          className="hover:text-pink-500 hover:font-medium active:scale-95 transition-all duration-200 overflow-hidden"
+        >
+          Home
+        </Link>
+        <Link
+          onClick={() => setIsOpen(false)}
+          to={"/generate"}
+          className="hover:text-pink-500 hover:font-medium active:scale-95 transition-all duration-200 overflow-hidden"
+        >
+          Generate
+        </Link>
+        <Link
+          onClick={() => setIsOpen(false)}
+          to={"/my-generation"}
+          className="hover:text-pink-500 hover:font-medium active:scale-95 transition-all duration-200 overflow-hidden"
+        >
+          My Generations
+        </Link>
+        <Link
+          onClick={() => setIsOpen(false)}
+          to={"#"}
+          className="hover:text-pink-500 hover:font-medium active:scale-95 transition-all duration-200 overflow-hidden"
+        >
+          My Contacts
+        </Link>
+        <Link
+          onClick={() => setIsOpen(false)}
+          to={"/login"}
+          className="hover:text-pink-500 hover:font-medium active:scale-95 transition-all duration-200 overflow-hidden"
+        >
+          Login
+        </Link>
         <button
           onClick={() => setIsOpen(false)}
           className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex"
