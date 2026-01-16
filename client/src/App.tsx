e0bb6462a2024ccfd5742b67898b306974d22d3c
Daksh Yadav <dakshdav@gmail.com>
2026-01-17 00:52:07 +0530

    Frontend completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Footer from "./components/Footer";
 import LenisScroll from "./components/LenisScroll";
 import Login from "./components/Login";
@@ -10,6 +11,9 @@ import MyGeneration from "./pages/MyGeneration";
 import YtPreview from "./pages/YtPreview";
 
 export default function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => window.scroll(0, 0), [pathname]);
   return (
     <>
       <LenisScroll />
@@ -19,7 +23,7 @@ export default function App() {
         <Route path="/generate" element={<Generate />} />
         <Route path="/generate/:id" element={<Generate />} />
         <Route path="/my-generation" element={<MyGeneration />} />
-        <Route path="/yt-preview" element={<YtPreview />} />
+        <Route path="/preview" element={<YtPreview />} />
         <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
