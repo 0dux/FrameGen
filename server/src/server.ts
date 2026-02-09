649bb3cabbfd02e8c4236752c5760b6c44073fb0
Daksh <dakshdav@gmail.com>
2026-02-09 09:59:57 +0530

    Fix: Remove commented-out debug logging middleware from server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,7 +22,6 @@ declare module "express-session" {
 const app = express();
 connectDB();
 
-// // Debug: Log ALL incoming requests (put FIRST before any middleware)
 // app.use((req, res, next) => {
 //     console.log(`>>> REQUEST: ${req.method} ${req.url}`);
 //     next();
